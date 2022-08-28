dbe6994910ad833fce3267ed685b92b0931e7b34
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2022-08-29 00:18:40 +0300

    Initial commit

---
+++ src/views/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+export const App = () => {
+  return (
+    <div className={styles.app}>
+      <main className={styles.main}>
+        <h1 className="visually-hidden">Тестовое задание</h1>
+      </main>
+    </div>
+  );
+};
